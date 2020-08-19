798a4ca4992fdde4e44d1b0f49a436d8c9ff4735
Ujjaval Sinha <39913625+ujjavalsinha@users.noreply.github.com>
2020-08-20 01:44:43 +0530

    Added basic arithmetic functionalities

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log("Hello World")
+let screen = document.getElementById('screen')
+let buttons = document.querySelectorAll('button')
+let screenValue = ''
+let validValues = '1234567890/*%().'
+console.log(buttons)
+for (button of buttons){
+    button.addEventListener('click',(e)=>{
+        buttonText = e.target.innerText
+        if(validValues.includes(buttonText) === false){
+            console.log("Not in list")
+            screenValue = ''
+            screen.value = screenValue;
+        }
+        else if (buttonText == '='){
+            screen.value = eval(screenValue)
+            screenValue = '';
+        }
+        else if(buttonText == 'C'){
+            screenValue = '';
+            screen.value = screenValue;
+        }
+        else{
+            screenValue+=buttonText;
+            screen.value=screenValue;
+            
+        }
+    })
+}
