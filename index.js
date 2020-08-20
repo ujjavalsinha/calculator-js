6015e0860ca51d744f9b3e140cade3265a64b749
SinhaUjj <ujjavaljolly@gmail.com>
2020-08-20 20:22:46 +0530

    Updated js and css

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
 let screen = document.getElementById('screen')
 let buttons = document.querySelectorAll('button')
 let screenValue = ''
-let validValues = '1234567890/*%().'
-console.log(buttons)
+let validValues = '1234567890/*%().+=-CE'
 for (button of buttons){
     button.addEventListener('click',(e)=>{
         buttonText = e.target.innerText
+        console.log(buttonText);
         if(validValues.includes(buttonText) === false){
             console.log("Not in list")
             screenValue = ''
             screen.value = screenValue;
         }
+        else if(buttonText == 'CE'){
+            if(screenValue.length != 0){
+                screenValue = screenValue.slice(0,screenValue.length-1)
+                screen.value = screenValue;
+            }
+        }
         else if (buttonText == '='){
             screen.value = eval(screenValue)
-            screenValue = '';
+            screenValue = ''
+            
         }
         else if(buttonText == 'C'){
             screenValue = '';
